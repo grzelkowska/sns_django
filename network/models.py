b69bf19a1470c13be6dcf7b20f5626ec2d1771d8
Phillip <philliplee515@gmail.com>
2022-05-12 13:29:06 +0900

    models.py init

--- network/models.py
+++ network/models.py
@@ -1,6 +1,36 @@
+from tokenize import blank_re
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Post(models.Model):
+    entry = models.TextField(max_length=512)
+    created_date = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT, related_name="all_creators")
+    image = models.ImageField(null=True, blank=True, upload_to="image/")
+
+    def __str__(self):
+        return f"{self.entry} by {self.creator}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} is following {self.following}"        
